c8bba2d14270f3a96f1258549885a3d35d2815e3
Brent Nef <b@n3f.co>
2018-09-25 15:34:15 -0700

    Adding entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
 	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 	# new topic
 	url(r'^new_topic/$', views.new_topic, name='new_topic'),
+	# Add entry
+	url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,10 +8,10 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 	
 def topics(request):
-	"""Show all topics."""
-	topics = Topic.objects.order_by('date_added')
-	context = {'topics': topics}
-	return render(request, 'learning_logs/topics.html', context)
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
 	
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
@@ -22,7 +22,7 @@ def topic(request, topic_id):
 
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def new_topic(request):
     """Add a new topic."""
@@ -37,3 +37,20 @@ def new_topic(request):
         return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+	
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)

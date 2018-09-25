8252dac7fb322b32a21b8bfc0ad9d5dd343ff394
Brent Nef <b@n3f.co>
2018-09-25 13:25:28 -0700

    after topic modelform

---
+++ learning_logs/urls.py
@@ -0,0 +1,15 @@
+"""Defines URL patterns for learning_logs."""
+from django.conf.urls import url
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    url(r'^$', views.index, name='index'),
+	# Topics page
+	url(r'^topics/$', views.topics, name='topics'),
+	# single topics
+	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+	# new topic
+	url(r'^new_topic/$', views.new_topic, name='new_topic'),
+]

25cfbbc61adc93791b3003e696186dff921db920
Brent Nef <b@n3f.co>
2018-09-25 18:24:26 -0700

    logout

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,15 @@
 """Defines url patterns for users."""
 
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView
+import django.contrib.auth.views as auth_views
 
 app_name = 'users'
 urlpatterns = [
     # Login page.
     url(r'^login/$',
-        LoginView.as_view(template_name='users/login.html'),
+        auth_views.LoginView.as_view(template_name='users/login.html'),
         name='login'),
+    url(r'^logout/$',
+        auth_views.LogoutView.as_view(next_page='learning_logs:index'),
+        name='logout'),
 ]
